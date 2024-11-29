3876e193030efd36027666fd5349d9074c4ca786
Jakub Witkowski <jakub.witkowski07@gmail.com>
2024-11-30 00:03:13 +0100

    fix for creating trading pairs with unique symbol

--- priv/repo/migrations/20241030202201_create_trading_pairs.exs
+++ priv/repo/migrations/20241030202201_create_trading_pairs.exs
@@ -7,5 +7,8 @@ defmodule ElixirBinanceBot.Repo.Migrations.CreateTradingPairs do
       add :last_price, :float
       timestamps()
     end
+
+    # Add a unique index on the :symbol column
+    create unique_index(:trading_pairs, [:symbol])
   end
 end
